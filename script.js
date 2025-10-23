b2b3610b819899e9825eb3fcaf8f47d8b4f67120
samian96 <sam.iannazzo@yahoo.com>
2025-10-23 00:05:03 -0700

    console.log answers

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log (8 * null); // equals to 0
+console.log("5" - 1); // equals to 4
+console.log("5" + 1); // equals to 51
+console.log("five" * 2); // equals to NaN 'not a number'
+console.log(false == 0); // equals to true
+console.log(5 > 2 && 6 > 3); // equals to true
+console.log(false == false ? "equal" : "not equal"); // equals to equal
+console.log(true || false); // equals to true
